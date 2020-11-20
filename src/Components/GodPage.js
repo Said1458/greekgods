8fca6c2187a5d819633717ef1b0e1fe85e834a2e
Inna Zilberov <70526642+silberov@users.noreply.github.com>
2020-11-20 11:13:39 +0100

    message change to class

---
+++ src/Components/GodPage.js
@@ -0,0 +1,136 @@
+import styled from 'styled-components';
+
+function GodPage(props) {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+    }
+
+    const Input = styled.input`
+        font-family: cinzel;
+        font-weight: bolder;
+        font-size: 16px;
+        background: #9EFFEA;
+        border: none;
+        margin-top: 16px;
+        padding: 8px 16px;
+        color: #1F2322;
+        border: solid 1px #9EFFEA;
+        &:hover {
+            background-color: transparent; 
+            border: solid 1px #9EFFEA;
+            color: #9EFFEA;
+          }
+          &:focus {
+            outline: none;
+         }
+          
+    `
+    const H2 = styled.h2`
+        font-family: cinzel;
+        font-size: 44px;
+        letter-spacing: 0.16px;
+        margin: auto;
+        text-align: center;
+        color: #9EFFEA;
+    `
+
+    const Description = styled.p`
+        max-width: 450px;
+        margin: auto;
+        font-family: raleway;
+    `
+    const Powers = styled.p`
+        max-width: 450px;
+        margin: 16px auto;
+        font-family: raleway;
+        font-weight: 700;
+    `
+    const GodForm = styled.form`
+        margin: 24px auto 0;
+        width: 100%;
+
+    `
+
+    const FormLabels = styled.label`
+        font-size: 12px;
+        text-align: left;
+        display: inline-block;
+        width: 70%
+       
+    `
+    const FormInputs = styled.input`
+        background-color: transparent;
+        font-family: raleway;
+        font-weight: 500;
+        letter-spacing: 0.2px;
+        color: #fff;
+        border: none;
+        border-bottom: solid 1px #fff;
+        width: 100%;
+        margin: 4px auto 8px;
+        font-size: 24px;
+        &:focus {
+            background-color: rgba(200, 200, 200, 0.1);
+            outline: none;
+            
+          }
+    `
+    const Textarea = styled.textarea`
+    background-color: transparent;
+    font-family: raleway;
+    font-weight: 500;
+    letter-spacing: 0.2px;
+    color: #fff;
+    border: none;
+    border-bottom: solid 1px #fff;
+    width: 100%;
+    height: 26px;
+    margin: 4px auto 8px;
+    font-size: 24px;
+    &:focus {
+        background-color: rgba(200, 200, 200, 0.1);
+        outline: none;
+        
+      }
+    `
+
+    const Message = styled.div`
+        background-color: #131515;
+        height: 100vh;
+        color: #fff;
+        text-align: center; 
+        display: flex;
+    `
+    return (
+    <>
+        <H2>{props.god.name}</H2>
+        <hr style={{width:"20%"}} />
+        <Powers>{props.god.power}</Powers>
+        <Description>She is the goddess of women, marriage, family and …he is the daughter of the Titans Cronus and Rhea.</Description>
+        <GodForm onSubmit={handleSubmit}>
+            <FormLabels>
+                Name:<br />
+                <FormInputs type="text" name="name" /> 
+            </FormLabels><br/>
+            
+            <FormLabels>
+                City:<br/>
+                <FormInputs type="text" name="name" />
+            </FormLabels><br/>
+
+            <FormLabels>
+                Prayer:<br/>
+                <Textarea /><br/>
+            </FormLabels><br/>
+                    
+            <Input type="submit" value="Submit" />
+        </GodForm>
+    </>
+
+    )
+
+}
+
+
+    export default GodPage;
